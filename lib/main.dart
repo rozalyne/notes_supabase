28e9965ba5999e1a567472742216718013044218
rozalyne <rickyerlangga.h@gmail.com>
2024-12-14 21:46:43 +0700

    DELETE ANON KEY

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,8 @@ void main() async {
   // Inisialisasi Supabase
   WidgetsFlutterBinding.ensureInitialized();
   await Supabase.initialize(
-    url: 'https://grhesycybpckhszleypm.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdyaGVzeWN5YnBja2hzemxleXBtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI5NjAyNTQsImV4cCI6MjA0ODUzNjI1NH0.UMad1FzMkbzwUHO42O1_0VS8x_8_INGBOYfNqnHtvCs',
+    url: '',
+    anonKey: '',
   );
   runApp(const MainApp());
 }
